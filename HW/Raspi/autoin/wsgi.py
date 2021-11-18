2ef42885bfae958dbb746668daa172494d2a2c01
kjaehyeon <jacob9404@gmail.com>
2021-11-18 18:49:23 +0900

    socketio done

--- HW/Raspi/autoin/wsgi.py
+++ HW/Raspi/autoin/wsgi.py
@@ -12,8 +12,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 import socketio
 from monitoring.views import sio
+import eventlet
+import eventlet.wsgi
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoin.settings')
 
 django_app = get_wsgi_application()
 application = socketio.WSGIApp(sio, django_app)
+eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
--- HW/Raspi/monitoring/views.py
+++ HW/Raspi/monitoring/views.py
@@ -8,9 +8,12 @@ import socketio
 from api.models import SensorValue, DayStatValue, MonthStatValue
 from datetime import datetime
 import json
+import eventlet
 
 basedir = os.path.dirname(os.path.realpath(__file__))
-sio = socketio.Server(async_mode=async_mode)
+eventlet.monkey_patch()
+sio = socketio.Server(async_mode='eventlet', cors_allowed_origins='*', cors_credentials=True)
+
 thread = None
 
 # def data_processing(data):
@@ -33,15 +36,13 @@ thread = None
 def index(request):
     global thread
     if thread is None:
-        thread = sio.start_background_task(background_thread)
-    return HttpResponse(status=200)
-    #return HttpResponse(open(os.path.join(basedir, 'static/index.html')))
+        thread = sio.start_background_task()
+    return Response(status=200)
 
 def index_test(request):
     global thread
     if thread is None:
         thread = sio.start_background_task(background_thread)
-    #return HttpResponse('connected!')
     return HttpResponse(open(os.path.join(basedir, 'static/index.html')))
 
 
@@ -51,41 +52,43 @@ def background_thread():
     while True:
         sio.sleep(10)
         count += 1
-        sio.emit('my_response', {'data': 'Server generated event'},
+        sio.emit('response', {'data': 'Server generated event'},
                  namespace='/test')
-
+# @sio.on('*')
+# def catch_all(event, sid, data):
+#     print('=================='+sid)
+    
 @sio.event
 def my_broadcast_event(sid, message):
-    sio.emit('my_response', {'data': message['data']})
+    sio.emit('response', {'data': message['data']})
     #data_processing(message['data'])
-
 #추후 다중 센서 디바이스 연결을 위한 함수
 @sio.event
 def my_event(sid, message):
-    sio.emit('my_response', {'data': message['data']}, room=sid)
+    sio.emit('response', {'data': message['data']}, room=sid)
 
 @sio.event
 def join(sid, message):
     sio.enter_room(sid, message['room'])
-    sio.emit('my_response', {'data': 'Entered room: ' + message['room']},
+    sio.emit('response', {'data': 'Entered room: ' + message['room']},
              room=sid)
 
 @sio.event
 def leave(sid, message):
     sio.leave_room(sid, message['room'])
-    sio.emit('my_response', {'data': 'Left room: ' + message['room']},
+    sio.emit('response', {'data': 'Left room: ' + message['room']},
              room=sid)
 
 @sio.event
 def close_room(sid, message):
-    sio.emit('my_response',
+    sio.emit('response',
              {'data': 'Room ' + message['room'] + ' is closing.'},
              room=message['room'])
     sio.close_room(message['room'])
 
 @sio.event
 def my_room_event(sid, message):
-    sio.emit('my_response', {'data': message['data']}, room=message['room'])
+    sio.emit('response', {'data': message['data']}, room=message['room'])
 
 @sio.event
 def disconnect_request(sid):
@@ -93,7 +96,7 @@ def disconnect_request(sid):
 
 @sio.event
 def connect(sid, environ):
-    sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
+    sio.emit('response', {'data': 'Connected'}, room=sid)
 
 @sio.event
 def disconnect(sid):
