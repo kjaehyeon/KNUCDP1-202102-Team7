fafe70ba239e0828ec6f9f3f460efbbe7e3e4367
Ktaewon <dydalehdxo@naver.com>
2021-11-21 15:05:06 +0900

    code cleaning

--- Public/JS/monitoring.js
+++ Public/JS/monitoring.js
@@ -29,14 +29,14 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
         type: 'GET',
         accepts: 'application/json',
         dataType: 'json',
-        success: function(data){
+        success: function(data) {
             const {
                 weather,
                 name,
-                main: {temp, humidity},
-                wind: {speed},
-                clouds: {all : cloud},
-                sys: {country}
+                main: { temp, humidity },
+                wind: { speed },
+                clouds: { all: cloud },
+                sys: { country }
             } = data;
             const description = weather[0].description;
             const icon = weather[0].icon;
@@ -67,7 +67,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                             </div>`
             $('.weather').append(element);
         },
-        error: function(request, status, error){
+        error: function(request, status, error) {
             Swal.fire({
                 title: 'Error',
                 html: `code: ${request.status}<br>message: ${request.responseText}<br>error: ${error}`,
@@ -94,25 +94,25 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                 borderColor: '#36a2eb',
                 borderWidth: 1,
             }],
-            
+
         },
         options: {
-                scales: {
-                    y: {
-                        min: 0,
-                        max: 40
-                    }
+            scales: {
+                y: {
+                    min: 0,
+                    max: 40
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
                 },
-                plugins: {
-                    legend: {
-                        display: false
-                    },
-                },
-            }
+            },
+        }
     });
-    
-    
-    $('.temperature').click(()=>{
+
+
+    $('.temperature').click(() => {
         chart1.data.datasets[0].data = temperature_array;
         chart1.data.datasets[0].backgroundColor = '#36a2eb';
         chart1.data.datasets[0].borderColor = '#36a2eb';
@@ -120,7 +120,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
         $('.realtime .title span').html('&nbsp;Realtime Temperature');
         chart1.update();
     })
-    $('.humidity').click(()=>{
+    $('.humidity').click(() => {
         chart1.data.datasets[0].data = humidity_array;
         chart1.data.datasets[0].backgroundColor = '#8fbe96';
         chart1.data.datasets[0].borderColor = '#8fbe96';
@@ -128,7 +128,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
         $('.realtime .title span').html('&nbsp;Realtime Humidity');
         chart1.update();
     })
-    $('.co').click(()=>{
+    $('.co').click(() => {
         chart1.data.datasets[0].data = co_array;
         chart1.data.datasets[0].backgroundColor = '#e04006';
         chart1.data.datasets[0].borderColor = '#e04006';
@@ -136,7 +136,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
         $('.realtime .title span').html('&nbsp;Realtime Co');
         chart1.update();
     })
-    $('.propane').click(()=>{
+    $('.propane').click(() => {
         chart1.data.datasets[0].data = propane_array;
         chart1.data.datasets[0].backgroundColor = '#f29d00';
         chart1.data.datasets[0].borderColor = '#f29d00';
@@ -145,23 +145,23 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
         chart1.update();
     })
     const socket = io(warehouse_info.iotServer);
-    
-    socket.on('connect',()=>{
-        if(socket.connected){
-            $('.left').click(()=>{
-                socket.emit('my_broadcast_event',{data: 'left'});
+
+    socket.on('connect', () => {
+        if (socket.connected) {
+            $('.left').click(() => {
+                socket.emit('my_broadcast_event', { data: 'left' });
             });
-            $('.right').click(()=>{
-                socket.emit('my_broadcast_event',{data: 'right'});
+            $('.right').click(() => {
+                socket.emit('my_broadcast_event', { data: 'right' });
             });
-            socket.on('response', (sensor)=>{
+            socket.on('response', (sensor) => {
                 let data = chart1.data.datasets[0].data;
                 const sensor_val = JSON.parse(sensor.data);
                 const temperature = sensor_val.temperature.toFixed(1);
                 const humidity = sensor_val.humidity.toFixed(1);
                 const co = Math.round(sensor_val.co);
                 const propane = Math.round(sensor_val.propane);
-                
+
                 temperature_array.unshift(temperature);
                 temperature_array.pop();
                 humidity_array.unshift(humidity);
@@ -170,7 +170,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                 co_array.pop();
                 propane_array.unshift(propane);
                 propane_array.pop();
-                
+
                 switch (graph) {
                     case 'temperature':
                         data = temperature_array;
@@ -178,7 +178,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                     case 'humidity':
                         data = humidity_array;
                         break;
-                        case 'co':
+                    case 'co':
                         data = co_array;
                         break;
                     case 'propane':
@@ -186,13 +186,13 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                         break;
                 }
                 chart1.update();
-                
+
                 $('.value')[0].innerHTML = `${temperature}&#8451;`;
                 $('.value')[1].innerHTML = `${humidity}%`;
                 $('.value')[2].innerHTML = `${co}ppm`;
                 $('.value')[3].innerHTML = `${propane}ppm`;
                 $('.value-wrapper')[4].classList.remove('fine', 'bad');
-                if(sensor_val.flame == 1){
+                if (sensor_val.flame == 1) {
                     $('.value-wrapper')[4].classList.add('fine');
                     $('.value')[4].innerHTML = 'FINE';
                 } else {
@@ -200,7 +200,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                     $('.value')[4].innerHTML = 'BAD';
                 }
                 $('.value-wrapper')[5].classList.remove('fine', 'bad');
-                if(sensor_val.vibration == 1){
+                if (sensor_val.vibration == 1) {
                     $('.value-wrapper')[5].classList.add('fine');
                     $('.value')[5].innerHTML = 'FINE';
                 } else {
@@ -208,7 +208,7 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                     $('.value')[5].innerHTML = 'BAD';
                 }
             })
-            $(window).on('beforeunload',()=>{
+            $(window).on('beforeunload', () => {
                 $('.left, .right').off();
                 socket.disconnect();
             })
@@ -216,11 +216,11 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
     })
     const useableArea = warehouse_info.useableArea;
     const usedArea = warehouse_info.usedArea;
-    
+
     const counter = {
         id: 'counter',
-        beforeDraw(chart, args, options){
-            const {ctx, chartArea: {top, right, bottom, left, width, height}} = chart;
+        beforeDraw(chart, args, options) {
+            const { ctx, chartArea: { top, right, bottom, left, width, height } } = chart;
             ctx.save();
 
             ctx.font = '600 24px sans-serif';
@@ -242,22 +242,22 @@ const initMonitoringDashboard = (api_key, warehouse_info) => {
                     '#F7685B',
                 ],
                 cutout: '70%',
-            }],    
+            }],
         },
         options: {
-                plugins: {
-                    legend: {
-                        position: 'right'
-                    },
+            plugins: {
+                legend: {
+                    position: 'right'
                 },
-                aspectRatio: 2,
-                yAxes: [{
-                    gridLines: {
-                        display: false,
-                        drawBorder: false,
-                    },
-                }],
             },
+            aspectRatio: 2,
+            yAxes: [{
+                gridLines: {
+                    display: false,
+                    drawBorder: false,
+                },
+            }],
+        },
         plugins: [counter],
     });
 };
