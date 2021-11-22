9e83422915bd6d257c7d88d1e09d51b97a91ba40
kjaehyeon <jacob9404@gmail.com>
2021-11-22 16:30:31 +0900

    make socket non-blocking

--- HW/Raspi/monitoring/views.py
+++ HW/Raspi/monitoring/views.py
@@ -44,16 +44,14 @@ def data_processing(parsed_data):
             tmp.save()
             
     #store statistic data of last month
-    #if(now.day == 1 and now.hour == 0 and now.minute == 1 and now.second == 0 ):
-    yesterday = now - timedelta(days=1)
-    if(not MonthStatValue.objects.filter(device_id=parsed_data["device_id"],datetime__year=yesterday.year, datetime__month=yesterday.month).exists()):
-        LastMonthValueList : DayStatValue = DayStatValue.objects.filter(device_id=parsed_data["device_id"],datetime__year=yesterday.year, datetime__month=yesterday.month)
-        aggreResult = LastMonthValueList.values('device_id').aggregate(Avg('temperature'), Avg('humidity'), Avg('co'), Avg('propane'))
-        tmp = MonthStatValue(device_id=parsed_data["device_id"],temperature=aggreResult['temperature__avg'], humidity=aggreResult['humidity__avg'],
-                        co=aggreResult['co__avg'], propane=aggreResult['propane__avg'], datetime=yesterday)
-        tmp.save()
-
-
+    if(now.day == 1 and now.hour == 0 and now.minute == 1 and now.second == 0 ):
+        yesterday = now - timedelta(days=1)
+        if(not MonthStatValue.objects.filter(device_id=parsed_data["device_id"],datetime__year=yesterday.year, datetime__month=yesterday.month).exists()):
+            LastMonthValueList : DayStatValue = DayStatValue.objects.filter(device_id=parsed_data["device_id"],datetime__year=yesterday.year, datetime__month=yesterday.month)
+            aggreResult = LastMonthValueList.values('device_id').aggregate(Avg('temperature'), Avg('humidity'), Avg('co'), Avg('propane'))
+            tmp = MonthStatValue(device_id=parsed_data["device_id"],temperature=aggreResult['temperature__avg'], humidity=aggreResult['humidity__avg'],
+                            co=aggreResult['co__avg'], propane=aggreResult['propane__avg'], datetime=yesterday)
+            tmp.save()
 
 class AuthSmsSend():
     EmgCount = 0
@@ -73,45 +71,11 @@ def notiEmg(data):
             }
             response = requests.get('http://192.168.0.17:5000/Api/Alert', headers=headers)
 
-
             AuthSmsSend.EmgCount = 0
         else:
             if (AuthSmsSend.pre == 1):
                 AuthSmsSend.EmgCount += 1
     else:
-        if (AuthSmsSend.pre == 1):
-            AuthSmsSend.EmgCount = 0
-        AuthSmsSend.pre = 0
-        return 
-
-class AuthSmsSend():
-    EmgCount = 0
-    pre = 0
-    time = None
-
-
-def notiEmg(data):
-    if data["flame"] == 0 or data["temperature"] > 100 or data["co"] > 200 or data["propane"] > 15 or data["vibration"] < 1000:
-        print("in notiEmg")
-        AuthSmsSend.pre = 1
-        ip = requests.get("https://api.ipify.org").text
-        if (AuthSmsSend.time is not None):
-            timeDiff = datetime.now() - AuthSmsSend.time
-        if AuthSmsSend.EmgCount >= 5 and (AuthSmsSend.time is None or timeDiff >= timedelta(hours = 1)):
-            headers ={
-                'Content-Type': 'application/json; charset=utf-8',
-                'client-ip' : f'http://{ip}:50000'
-            }
-            response = requests.get('http://192.168.0.17:5000/Api/Alert', headers=headers)
-            print(response.status_code, type(response.content))
-
-
-            AuthSmsSend.EmgCount = 0
-        else:
-            if (AuthSmsSend.pre == 1):
-                AuthSmsSend.EmgCount += 1
-    else:
-        print("pass")
         if (AuthSmsSend.pre == 1):
             AuthSmsSend.EmgCount = 0
         AuthSmsSend.pre = 0
@@ -159,10 +123,11 @@ def my_broadcast_event(sid, message):
 def camera_move(sid, message):
     orientation = message['data']
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.setblocking(False)
     try:
         clientsocket.connect(("192.168.0.21", 1234))
-    except socket.error:
-        print('error')
+    except socket.error or BlockingIOError:
+        clientsocket.close()
         pass
     if(orientation == 'r'):
         clientsocket.send((orientation+'\n').encode('utf-8'))
