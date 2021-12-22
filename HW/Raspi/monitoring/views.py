b18690d69c0596b34d177b8547e7e2e4084520f5
kjaehyeon <jacob9404@gmail.com>
2021-12-22 15:40:39 +0900

    separate env var

--- HW/Raspi/monitoring/views.py
+++ HW/Raspi/monitoring/views.py
@@ -71,38 +71,8 @@ def notiEmg(data):
                 'Content-Type': 'application/json; charset=utf-8',
                 'client-ip' : f'http://{ip}:50000'
             }   
-            response = requests.get('http://3.38.92.244:5000/Api/Alert', headers=headers)
+            response = requests.get('http://'+os.environ.get('SERVER_IP')+'/Api/Alert', headers=headers)
 
-
-            AuthSmsSend.EmgCount = 0
-        else:
-            if (AuthSmsSend.pre == 1):
-                AuthSmsSend.EmgCount += 1
-    else:
-        if (AuthSmsSend.pre == 1):
-            AuthSmsSend.EmgCount = 0
-        AuthSmsSend.pre = 0
-        return 
-
-class AuthSmsSend():
-    EmgCount = 0
-    pre = 0
-    time = None
-
-def notiEmg(data):
-    if data["flame"] == 0 or data["temperature"] > 100 or data["co"] > 200 or data["propane"] > 15 or data["vibration"] < 1000:
-        AuthSmsSend.pre = 1
-        ip = requests.get("https://api.ipify.org").text
-        if (AuthSmsSend.time is not None):
-            timeDiff = datetime.now() - AuthSmsSend.time
-        if AuthSmsSend.EmgCount >= 5 and (AuthSmsSend.time is None or timeDiff >= timedelta(hours = 1)):
-            print(AuthSmsSend.EmgCount, AuthSmsSend.pre, AuthSmsSend.time)
-            AuthSmsSend.time = datetime.now()
-            headers ={
-                'Content-Type': 'application/json; charset=utf-8',
-                'client-ip' : f'http://{ip}:50000'
-            }
-            response = requests.get('http://192.168.0.17:5000/Api/Alert', headers=headers)
 
             AuthSmsSend.EmgCount = 0
         else:
@@ -158,7 +128,7 @@ def camera_move(sid, message):
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print(orientation)
     try:
-        clientsocket.connect(("192.168.0.21", 1234))
+        clientsocket.connect((os.environ.get('ARDUINO_IP'), 1234))
         if(orientation == 'r'):
             clientsocket.send((orientation+'\n').encode('utf-8'))
         elif(orientation == 'l'):
