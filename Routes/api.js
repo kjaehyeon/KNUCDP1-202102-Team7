0db275084fd19c40bd52dc09584462a05516618d
hyunjun <nhj2927@naver.com>
2022-01-03 15:22:04 +0900

    add login api

--- Routes/api.js
+++ Routes/api.js
@@ -2,9 +2,14 @@ module.exports = (app, pool) => {
     const express = require('express');
     const router = express.Router();
     const alert = require('./api_Alert');
+    const user = require('./api_User');
 
     router.get('/alert', async (req, res) => {
         await alert(req, res, pool);
+    });
+
+    router.post('/login', async (req, res) => {
+        await user.login(req, res, pool);
     });
 
     return router;  
